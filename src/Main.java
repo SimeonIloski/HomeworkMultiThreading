a7ea2e1071f74f9ee0ca76b6e7ad507bf94d6ce4
SimeonIloski <simeoniloski@gmail.com>
2019-07-16 15:20:24 +0200

    Added NewThread,Person.java for thread local example for primitive types,custom objects in a synchronized block

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,34 @@ public class Main {
         ReentrantLockTest();
        //call of test method for threads with ReentrantReadWriteLock
         reentrantreadWriteLockTest();
+       //call of method for testing a thread local, primitive type variable and custom object in synchronized block
+       threadLocalTypeTest();
 
 
+    }
+
+    private static void threadLocalTypeTest() {
+        NewThread newThread1 = new NewThread();
+        NewThread newThread2 = new NewThread();
+        NewThread newThread3 = new NewThread();
+        NewThread newThread4 = new NewThread();
+        NewThread newThread5 = new NewThread();
+        newThread1.start();
+        newThread2.start();
+        newThread3.start();
+        newThread4.start();
+        newThread5.start();
+        newThread1.run();
+        newThread2.run();
+        newThread3.run();
+        newThread4.run();
+        newThread5.run();
+        newThread1.stop();
+        newThread2.stop();
+        newThread3.stop();
+        newThread3.stop();
+        newThread4.stop();
+        newThread5.stop();
     }
 
     private static void ReentrantLockTest(){
