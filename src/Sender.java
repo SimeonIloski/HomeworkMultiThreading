d267dc305cb472d1686a8aae542aa555b792be97
zsrbakova <zsrbakova@yahoo.com>
2019-07-17 11:18:47 +0200

    added second example for thread local and synchronized block

---
+++ src/Sender.java
@@ -0,0 +1,22 @@
+// A Java program to demonstrate working of
+// synchronized.
+import java.io.*;
+import java.util.*;
+
+// A Class used to send a message
+class Sender
+{
+    public void send(String msg)
+    {
+        System.out.println("Sending\t"  + msg );
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Thread  interrupted.");
+        }
+        System.out.println("\n" + msg + "Sent");
+    }
+}
